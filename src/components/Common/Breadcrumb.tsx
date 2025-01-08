d93f246c826ddb09cd0248f30129fd180e85c269
viskar07 <viskar.code@gmail.com>
2025-01-08 12:39:07 +0530

    update5

--- src/components/Common/Breadcrumb.tsx
+++ src/components/Common/Breadcrumb.tsx
@@ -25,12 +25,12 @@ const Breadcrumb = ({
               <div className="text-end">
                 <ul className="flex items-center md:justify-end">
                   <li className="flex items-center">
-                    <Link
+                    <a
                       href="/"
                       className="pr-1 text-base font-medium text-body-color hover:text-primary"
                     >
                       Home
-                    </Link>
+                    </a>
                     <span className="mr-3 block h-2 w-2 rotate-45 border-r-2 border-t-2 border-body-color"></span>
                   </li>
                   <li className="text-base font-medium text-primary">
